55947625ca42d43f573fed471b8051a115f99818
Salvador-Mendiola <pangu25.smt@gmail.com>
2024-02-01 14:10:16 -0600

    v2,0

--- index.php
+++ index.php
@@ -2,5 +2,17 @@
     require_once realpath('./vendor/autoload.php');
     $dotenv=Dotenv\Dotenv::createImmutable('./');
     $dotenv->load();
-    echo $_ENV['MI_VARIABLE_ENTORNO'];
+    $puerto = $_ENV['PUERTO'];
+    $usuario = $_ENV['USUARIO'];
+    $bd = $_ENV['BD'];
+    $password = $_ENV['PASSWORD'];
+    $host = $_ENV['HOST'];
+    $conexion = new PDO("mysql:host=$host;port=$puerto;dbname=$bd",$usuario,$password);
+
+    if ($conexion != True) {
+        echo "No se pudo conectar :(";
+    }else {
+        echo "Conectado :3";
+    }
+
 ?>
